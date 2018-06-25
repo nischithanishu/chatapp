d46cd68c28ccd28b8e3e677f60425f7cafcb2c78
nischithanishu <nishithagy@gmail.com>
2018-06-25 14:41:00 +0530

    tap message

--- public/js/index.js
+++ public/js/index.js
@@ -23,4 +23,26 @@ console.log('disconnected from server');
 
 socket.on('newMessage',function(message){
 console.log('newMessage',message);
+var li = jQuery('<li></li>');
+li.text(`${message.from}: ${message.text}`);
+
+jQuery('#message').append(li);
+});
+
+// socket.emit('createMessage',{
+//
+// from:'teju',
+// text:'hhiiiiiiii helllooo'
+// },function(data){
+//   console.log('got it',data);
+// });
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function (){
+
+  });
 });
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT||3000;
 
 //console.log(__dirname+'/../public');
-//console.log(publicPath);
+
 var app=express();
 var server=http.createServer(app);
 var io=socketIO(server);
@@ -18,34 +18,17 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
 console.log('new user connected');
 
- socket.emit('newMessage',generateMessage('admin','welcome to chat app'));
+   socket.emit('newMessage',generateMessage('admin','welcome to chat app'));
 
+   socket.broadcast.emit('newMessage',generateMessage('admin','new member joined'));
 
+   socket.on('createMessage',(message,callback)=>{
+      console.log('create message',message);
+      io.emit('newMessage',generateMessage(message.from,message.text));
+       callback('this is from server');
 
-  socket.broadcast.emit('newMessage',generateMessage('admin','new member joined'));
-
-
-
-
-
-
-
-socket.on('createMessage',(message)=>{
-console.log('create message',message);
- io.emit('newMessage',generateMessage(message.from,message.text));
-// from:message.from,
-// text:message.text,
-// createdAt:new Date().getTime()
-// });
 });
 
-//
-// socket.broadcast.emit('newMessage',{
-//   from:message.from,
-//   text:message.text,
-//   createAt:new Date().getTime()
-// });
-// });
 
 socket.on('disconnect',()=>{
 console.log('user was disconnected');
