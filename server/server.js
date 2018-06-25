e7dd0775b4908defb0830292842e4b1e8ddc309f
nischithanishu <nishithagy@gmail.com>
2018-06-25 12:07:38 +0530

    tab message

--- server/server.js
+++ server/server.js
@@ -1,13 +1,51 @@
 const path=require('path');
-const express=require('express')
+const http=require('http');
+const express=require('express');
+const socketIO=require('socket.io');
+
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT||3000;
 
-//
-// console.log(__dirname + '/../public');
-// console.log(publicPath);
+//console.log(__dirname+'/../public');
+//console.log(publicPath);
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
+
 app.use(express.static(publicPath));
-app.listen(port,()=>{
-  console.log(`server is up to port ${port}`);
+
+io.on('connection',(socket)=>{
+console.log('new user connected');
+
+// socket.emit('newMessage',{
+// from:'pooja',
+// text:'see you then',
+// createdAt:1234
+// });
+// socket.emit('newEmail',{
+// from:'pooja.h.gowda@gmail.com',
+// text:'hey this is pooja',
+// createAt:123
+// });
+
+// socket.on('createEmail',(newEmail)=>{
+// console.log('createEmail',newEmail);
+// });
+
+socket.on('createMessage',(message)=>{
+console.log('create message',message);
+io.emit('newMessage',{
+from:message.from,
+text:message.text,
+createdAt:new Date().getTime()
+});
+});
+
+socket.on('disconnect',()=>{
+console.log('user was disconnected');
+});
+});
+
+server.listen(port,()=>{
+console.log(`server is up on ${port}`);
 });
